e33fce5c34d395273512a8c4331d345828e8c498
Svyatoslav <svyatoslav.kasian4uk@gmail.com>
2024-03-02 16:13:54 +0200

    add subtraction operation

--- .venv/Scripts/Calculator.py
+++ .venv/Scripts/Calculator.py
@@ -5,6 +5,11 @@ class Calculator:
     def add(a, b):
         return a + b
 
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         # Arrange
@@ -17,6 +22,19 @@ class TestCalculator(unittest.TestCase):
 
         # Assert
         self.assertEqual(result, expected_sum, "Sum is not calculated correctly")
+
+    def test_subtract(self):
+        # Arrange
+        a = 10
+        b = 3
+        expected_difference = 7
+
+        # Act
+        result = Calculator.subtract(a, b)
+
+        # Assert
+        self.assertEqual(result, expected_difference, "Subtraction is not calculated correctly")
+
 
 if __name__ == '__main__':
     unittest.main()

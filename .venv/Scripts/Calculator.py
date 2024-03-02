dd4dedf507c7d33238f4fe4d344cdf6fade092c2
Svyatoslav <svyatoslav.kasian4uk@gmail.com>
2024-03-02 16:12:19 +0200

    add addition operation

--- .venv/Scripts/Calculator.py
+++ .venv/Scripts/Calculator.py
@@ -1,2 +1,22 @@
+import unittest
+
 class Calculator:
-    return
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        # Arrange
+        a = 3
+        b = 5
+        expected_sum = 8
+
+        # Act
+        result = Calculator.add(a, b)
+
+        # Assert
+        self.assertEqual(result, expected_sum, "Sum is not calculated correctly")
+
+if __name__ == '__main__':
+    unittest.main()
